2e5d8bef1109e06a90c09c691c7c2e346c714ab7
quikstrike <quikstrik3@gmail.com>
2015-10-20 15:42:02 -0500

    changes to work with local data.json file

--- knexfile.js
+++ knexfile.js
@@ -19,10 +19,6 @@ module.exports = {
   production: {
     client: 'postgresql',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       tableName: 'knex_migrations'
     }
