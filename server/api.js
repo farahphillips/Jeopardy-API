a4eb804cec02802575f50f3974fe5211d9d4ad81
fmaredia <farhana.maredia@gmail.com>
2015-10-22 11:05:50 -0500

    lays out what routes will be created

--- server/api.js
+++ server/api.js
@@ -1,14 +1,74 @@
 module.exports = function(app) {
-  app.get('/questions', function (req, res) {
-  	res.send('hello world')
+
+  app.get('/board', function (req, res) {
+  	if (err) {
+  		console.log('error')
+  	} else {
+      // returns a board full of questions
+     	res.json()
+  	}	
   })
 
-  //all episodes (/episodes)
-  //all categories (/categories)
-  //all finalJeopardy
-  //all questions of a certain category (/questions/:category)
-  //all questions from an episode (/questions/:episode)
-  //all questions with media (/questions/media)
-  //all questions without media (/questions/nomedia)
-  //all questions from a date range (/questions/date/[object])
+  app.get('/board/nomedia', function (req, res) {
+  	if (err) {
+  		console.log('error')
+  	} else {
+  		// returns a board without media
+     	res.json()
+  	}	
+  })
+
+  app.get('/episodes', function (req, res) {
+  	if (err) {
+  		console.log('error')
+  	} else {
+  		// returns list of episodes
+     	res.json()
+  	}	
+  })
+  
+  app.get('/categories', function (req, res) {
+  	if (err) {
+  		console.log('error')
+  	} else {
+  		// returns list of categories
+     	res.json()
+  	}	
+  })
+
+  app.get('/final', function (req, res) {
+  	if (err) {
+  		console.log('error')
+  	} else {
+  		// returns all final jeopardy questions
+     	res.json()
+  	}	
+  })
+  
+  app.get('/questions/:category', function (req, res) {
+  	if (err) {
+  		console.log('error')
+  	} else {
+  		// returns all questions of a specified category
+     	res.json()
+  	}	
+  })
+  
+  app.get('/questions/:episode', function (req, res) {
+  	if (err) {
+  		console.log('error')
+  	} else {
+  		// returns all questions from a specified episode
+     	res.json()
+  	}	
+  })
+
+  app.get('/questions/date/{}', function (req, res) {
+  	if (err) {
+  		console.log('error')
+  	} else {
+  		// returns all questions from a date range
+     	res.json()	
+  	}	
+  })
 }
