50e91c107fecd1eac954348f81b387142d425c27
quikstrike <quikstrik3@gmail.com>
2015-10-24 00:27:03 -0500

    Removed Temp Fix, and added indexes for categories, and episode numbers

--- test/server/index_test.js
+++ test/server/index_test.js
@@ -1,4 +1,5 @@
 var request = require('supertest')
+var assert  = require('assert')
 var routes = require(__server + '/index.js')
 
 describe("The Server", function() {
@@ -7,12 +8,19 @@ describe("The Server", function() {
   app.use('/', routes)
   app.testReady()
 
-  it("Should Respond With JSON", function(done) {
+  it("GET /api/questions/random responds with a random question", function(done) {
 
     // Mocha will wait for returned promises to complete
     return request(app)
       .get('/api/questions/random')
       .set('Accept', 'application/json')
-      .expect(200,done)
+      .expect(200)
+      .expect(function(response) {
+        var res = response.body
+        assert.equal(typeof res.question, "string", "question should defined");
+        assert.equal(typeof res.answer, "string", "answer should defined");
+        assert.equal(typeof res.value, "number", "number should defined");
+      })
+      .end(done)
   })
 })
