e08482a390f84de0247f200ffc925c195e2a88d4
fmaredia <farhana.maredia@gmail.com>
2015-11-17 15:27:35 -0600

    Implements questions/episode/:id and test, closes #6

--- server/api.js
+++ server/api.js
@@ -34,13 +34,19 @@ routes.get('/finaljeopardy', function (req, res) {
 })
 
 //all questions of a certain category id (/questions/:id)
-routes.get('/questions/:id', function (req, res) {
+routes.get('/questions/category/:id', function (req, res) {
   Question.findAllBy('category_id', req.params.id).then(function(questions){
     res.send(questions)
   })
-}) 
+})
 
 //all questions from an episode (/questions/:episode)
+routes.get('/questions/episode/:id', function (req, res) {
+  Question.findAllBy('episode_number_id', req.params.id).then(function(questions){
+    res.send(questions)
+  })
+})
+
 //all questions with media (/questions/media)
 //all questions without media (/questions/nomedia)
 //all questions from a date range (/questions/date/[object])
--- test/server/index_test.js
+++ test/server/index_test.js
@@ -64,9 +64,9 @@ describe("The Server", function() {
       .end(done)
   })
 
-  it("GET /api/questions/:id responds with all questions of a specified category id", function(done) {
+  it("GET /api/questions/category/:id responds with all questions of a specified category id", function(done) {
     return request(app)
-      .get('/api/questions/1')
+      .get('/api/questions/category/1')
       .set('Accept', 'application/json')
       .expect(200)
       .expect(function(response) {
@@ -74,6 +74,21 @@ describe("The Server", function() {
         for (var i = 0; i < res.length; i++) {
           assert.equal(res[i].category_id, 1, "Question's category_id value should be 1");
           assert.equal(typeof res[i].category_id, "number", "Question category_id should be a number");
+        }
+      })
+      .end(done)
+  })
+
+  it("GET /api/questions/episode/:id responds with all questions of a specified episode number id", function(done) {
+    return request(app)
+      .get('/api/questions/episode/1')
+      .set('Accept', 'application/json')
+      .expect(200)
+      .expect(function(response) {
+        var res = response.body
+        for (var i = 0; i < res.length; i++) {
+          assert.equal(res[i].episode_number_id, 1, "Question's episode_number_id value should be 1");
+          assert.equal(typeof res[i].episode_number_id, "number", "Question episode_number_id should be a number");
         }
       })
       .end(done)
